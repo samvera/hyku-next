51d9b28090bd759f50ee5b53253bc7d449c558bc
Adam Joseph Arling <aarling@gmail.com>
2024-05-03 09:23:19 -0500

    Update config to get static site paths working

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  experimental: {
+    esmExternals: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   output: "export",
 };
 
