3b8886465ba7616dbf8ec1e8f791f58bd033c138
Adam Joseph Arling <aarling@gmail.com>
2024-05-24 12:09:28 -0500

    Update Next config for base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: "",
+  basePath: process.env.NODE_ENV === "production" ? "/hyku-next/" : "",
   experimental: {
     esmExternals: true,
   },
