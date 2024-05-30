e1885b153299ac1537e7fc2b989fee4ac6595140
Adam Joseph Arling <aarling@gmail.com>
2024-05-30 11:34:30 -0500

    Render line breaks in Contentful Rich Text

--- src/components/rich-text-content.tsx
+++ src/components/rich-text-content.tsx
@@ -2,7 +2,17 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 const RichTextContent = ({ content }: { content: any }) => {
   if (!content) return null;
-  return <>{documentToReactComponents(content)}</>;
+
+  const options = {
+    // Helper function to handle line breaks in rich text
+    renderText: (text: string) => {
+      return text.split("\n").reduce((children: any, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment];
+      }, []);
+    },
+  };
+
+  return <>{documentToReactComponents(content, options)}</>;
 };
 
 export default RichTextContent;
