d944d22f673ed91829c9ad6fb1fc27232df325d6
Heather Greer Klein <heather@samvera.org>
2025-07-15 23:06:32 -0400

    Update hosted-solutions.tsx
    removed lorum ipsum

--- src/components/hosted-solutions.tsx
+++ src/components/hosted-solutions.tsx
@@ -26,8 +26,7 @@ export default function HostedSolutions() {
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2>Hosted Solutions</h2>
           <p className="mt-6 text-lg leading-8 text-foreground-muted">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-            voluptatum cupiditate veritatis in accusamus quisquam.
+            Service providers offering hosted Hyku solutions and migration assistance. 
           </p>
         </div>
         <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
