64b3a446487f74e4bf75f088049589a43f02018d
Heather Greer Klein <heather@samvera.org>
2025-07-15 23:10:36 -0400

    Update hosted-solutions.tsx
    removed outdated entries

--- src/components/hosted-solutions.tsx
+++ src/components/hosted-solutions.tsx
@@ -1,20 +1,11 @@
 import Container from "@/components/layout/container";
 
 const hostedSolutions = [
-  {
-    name: "HykuUP",
-    description: "by SoftServ by Scientist.com",
-    url: "https://hykuup.com/",
-  },
-  {
-    name: "Ubiquity Repositories",
-    description: "by Ubiquity Press",
-    url: "https://ubiquity.pub/repositories/",
-  },
+  
   {
     name: "Service Providers page",
     description:
-      "For hosted solutions, Requests for Proposals, or contracted work",
+      "See the Service Providers page on samvera.org to find hosted solutions, send Requests for Proposals, or explore contracted work",
     url: "https://samvera.org/the-community/service-providers",
   },
 ];
@@ -26,7 +17,7 @@ export default function HostedSolutions() {
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2>Hosted Solutions</h2>
           <p className="mt-6 text-lg leading-8 text-foreground-muted">
-            Service providers offering hosted Hyku solutions and migration assistance. 
+            Service providers offering hosted Hyku solutions. 
           </p>
         </div>
         <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
