b5020686e9d1cb26c4d8effd856ddfaf151cc97a
jrgriffiniii <1443986+jrgriffiniii@users.noreply.github.com>
2025-08-13 11:36:44 -0400

    Updating the broken acceptance test suite (e. g. ensuring that the test suite no longer expects a link to twitter.com for Hyku)

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,12 @@ dotenv.config();
 const nextConfig = {
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  contentfulSpaceID: process.env.NEXT_CONTENTFUL_SPACE_ID,
-  contentfulAccessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
-  contentfulMgmtAccessToken: process.env.NEXT_CONTENTFUL_CMA_TOKEN,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+    NEXT_CONTENTFUL_SPACE_ID: process.env.NEXT_CONTENTFUL_SPACE_ID,
+    NEXT_CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
+    NEXT_CONTENTFUL_CMA_TOKEN: process.env.NEXT_CONTENTFUL_CMA_TOKEN,
+  },
   eslint: {
     dirs: ["src"],
   },
